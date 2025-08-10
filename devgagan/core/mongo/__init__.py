32305369676795c08725fc1d3584b548a6dc3d6e
sahil140708 <sahilarmy10@gmail.com>
2025-08-10 10:56:28 +0530

    Add files via upload

---
+++ devgagan/core/mongo/__init__.py
@@ -0,0 +1,12 @@
+# ---------------------------------------------------
+# File Name: __init__.py
+# Description: A Pyrogram bot for downloading files from Telegram channels or groups 
+#              and uploading them back to Telegram.
+# Author: MrHacker
+# GitHub: https://github.com/mrhacker011106
+# Telegram: @scr007Botupdates
+# Created: 2025-01-11
+# Last Modified: 2025-01-11
+# Version: 2.0.5
+# License: MIT License
+# ---------------------------------------------------
